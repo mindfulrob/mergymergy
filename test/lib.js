2daa4b7941cfc8fd95effc5347586a1f6fe796fe
Rob Anderson <robandpdx@github.com>
2025-08-07 11:15:08 -0700

    A: Firm up check in calculate()

--- test/lib.js
+++ test/lib.js
@@ -6,6 +6,10 @@ describe('lib', () => {
     it('two numbers', () => {
       assert.equal(calculate(1, 1), 2);
     });
+
+    it('error if zero or null number', () => {
+      assert.throws(() => { return calculate(1) });
+    });
   });
 });
 
